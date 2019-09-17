ce23b924b8f67bcdf5d60879a584155164a4049b
conorbailey90 <conbailey90@gmail.com>
2019-09-17 19:16:54 +0100

    JS Win checks added and CSS updated

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const resetBtn = document.querySelector('.reset');
 
 
 var currentPlayer = 1;
+let winner;
 playerTurn.textContent = `${player1}'s turn!`
 
 // Log cell coordinates when clicked
@@ -32,14 +33,6 @@ for (i = 0; i < tableData.length; i ++){
 
 // Funtions
 
-function changePlayer(){
-    if (currentPlayer === 1){
-        return currentPlayer = 2;
-    }else{
-        return currentPlayer = 1;
-    }
-}
-
 function changeColor(e){
     // Get clicked column index
     let column = e.target.cellIndex;
@@ -49,15 +42,28 @@ function changeColor(e){
         if (tableRow[i].children[column].style.backgroundColor == 'white'){
             row.push(tableRow[i].children[column]);
             if (currentPlayer === 1){
-                row[0].style.backgroundColor = player1Color;
-                playerTurn.textContent = `${player2}'s turn`
-                return currentPlayer = 2;
+                row[0].style.backgroundColor = 'red';
+                if (horizontalCheck() || verticalCheck() || diagonalCheck() || diagonalCheck2()){
+                    playerTurn.textContent = `${player1} WINS!!`;
+                    playerTurn.style.color = player1Color;
+                    return alert(`${player1} WINS!!`);
+                }else{
+                    playerTurn.textContent = `${player2}'s turn`
+                    return currentPlayer = 2;
+                }
             }else{
-                row[0].style.backgroundColor = player2Color;
-                playerTurn.textContent = `${player1}'s turn`;
-                return currentPlayer = 1;
+                row[0].style.backgroundColor = 'yellow';
+                if (horizontalCheck() || verticalCheck() || diagonalCheck() || diagonalCheck2()){
+                    playerTurn.textContent = `${player2} WINS!!`;
+                    playerTurn.style.color = player2Color;
+                    return alert(`${player2} WINS!!`);
+                }else{
+                    playerTurn.textContent = `${player1}'s turn`;
+                    return currentPlayer = 1;
+                }
+                
             }
-        };
+        }
     }
 }
 
@@ -65,11 +71,61 @@ Array.prototype.forEach.call(tableData, (cell) => {
     cell.addEventListener('click', changeColor);
     // Set all slots to white for new game.
     cell.style.backgroundColor = 'white';
-})
+});
 
+function colorMatchCheck(one, two, three, four){
+    return (one === two && one === three && one === four && one !== 'white' && one !== undefined);
+}
+
+function horizontalCheck(){
+    for (let row = 0; row < tableRow.length; row++){
+        for (let col =0; col < 4; col++){
+           if (colorMatchCheck(tableRow[row].children[col].style.backgroundColor,tableRow[row].children[col+1].style.backgroundColor, 
+                                tableRow[row].children[col+2].style.backgroundColor, tableRow[row].children[col+3].style.backgroundColor)){
+               return true;
+           }
+        }
+    }
+}
+
+function verticalCheck(){
+    for (let col = 0; col < 7; col++){
+        for (let row = 0; row < 3; row++){
+            if (colorMatchCheck(tableRow[row].children[col].style.backgroundColor, tableRow[row+1].children[col].style.backgroundColor,
+                                tableRow[row+2].children[col].style.backgroundColor,tableRow[row+3].children[col].style.backgroundColor)){
+                return true;
+            };
+        }   
+    }
+}
+
+function diagonalCheck(){
+    for(let col = 0; col < 4; col++){
+        for (let row = 0; row < 3; row++){
+            if (colorMatchCheck(tableRow[row].children[col].style.backgroundColor, tableRow[row+1].children[col+1].style.backgroundColor,
+                tableRow[row+2].children[col+2].style.backgroundColor,tableRow[row+3].children[col+3].style.backgroundColor)){
+                    return true;
+                }
+            }
+        }
+
+}
+
+function diagonalCheck2(){
+    for(let col = 0; col < 4; col++){
+        for (let row = 5; row > 2; row--){
+            if (colorMatchCheck(tableRow[row].children[col].style.backgroundColor, tableRow[row-1].children[col+1].style.backgroundColor,
+                tableRow[row-2].children[col+2].style.backgroundColor,tableRow[row-3].children[col+3].style.backgroundColor)){
+                    return true;
+            }
+        }
+    }
+}
 
 resetBtn.addEventListener('click', () => {
     slots.forEach(slot => {
         slot.style.backgroundColor = 'white';
-    })
+    });
+    playerTurn.style.color = 'black';
+    return (currentPlayer === 1 ? playerTurn.textContent = `${player1}'s turn` : playerTurn.textContent = `${player2}'s turn`);
 });
